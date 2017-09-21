3e578e8df0ea13bcb7fd265d179d407af85a3dd0
Ron Magno <ronzohan@rons-imac.local>
2017-09-21 22:01:51 +0800

    Added properties

--- Reddit/Modules/DataLayer/Models/Link.swift
+++ Reddit/Modules/DataLayer/Models/Link.swift
@@ -12,31 +12,74 @@ import ObjectMapper
 
 enum LinkKeys: String {
 	case author
-	case title
-	case url
 	case authorFlairCSSClass = "author_flair_css_class"
+	case authorFlairText = "author_flair_text"
+	case clicked
+	case domain
+	case hidden
+	case isSelf = "is_self"
+	case linkFlairCSSClass = "link_flair_css_class"
+	case linkFlairText = "link_flair_text"
+	case locked
+	case numComments = "num_comments"
+	case over18 = "over_18"
+	case permalink
+	case preview
+	case saved
+	case score
+	case selftext
+	case selftextHTML = "selftext_html"
+	case subreddit
+	case subredditID = "subreddit_id"
+	case subredditNamePrefixed = "subreddit_name_prefixed"
+	case stickied
 	case thumbnail
 	case thumbnailHeight = "thumbnail_height"
-	case preview
+	case title
+	case url
 }
 
 class Link: Votable, Created, Thing, Mappable {
-	var kind: Kind = .link
-
-	var id: String = ""
-	var name: String = ""
-    var author: String?
-    var title: String = ""
-    var url: String = ""
-    var authorFlairCSSClass: String?
+    var author: String = ""
+	var authorFlairCSSClass: String?
+	var authorFlairText: String?
+	var clicked: Bool = false
+	var domain: String = ""
+	var distinguished = false
+	var edited: Bool = false
+	var hidden: Bool = false
+	var isSelf: Bool = false
+	var linkFlairCSSClass: String?
+	var linkFlairText: String?
+	var locked: Bool = false
+	// TODO: var media
+	// TODO: var mediaEmbed
+	var numComments: Int = 0
+	var over18: Bool = false
+	var permalink: String = ""
+	var preview: PreviewImage = PreviewImage()
+	var saved: Bool = false
+	var score: Int = 0
+	var selftext: String = ""
+	var selftextHTML: String?
+	var stickied: Bool = false
+	var subreddit: String = ""
+	var subredditID: String = ""
+	var title: String = ""
+	var subredditNamePrefixed: String = ""
 	var thumbnail: String?
 	var thumbnailHeight: Double = 0
-	var preview: PreviewImage = PreviewImage()
+	var url: String = ""
+
+	// Thing
+	var id: String = ""
+	var kind: Kind = .link
+	var name: String = ""
 
 	// Votable
-	var ups: Int = 0
 	var downs: Int = 0
-	var likes: Bool = false
+	var likes: Bool?
+	var ups: Int = 0
 
 	// Created
 	var created: UInt64 = 0
@@ -51,17 +94,36 @@ class Link: Votable, Created, Thing, Mappable {
 		name <- map[ThingKeys.name.rawValue]
 
 		author <- map[LinkKeys.author.rawValue]
-		title <- map[LinkKeys.title.rawValue]
-		url <- map[LinkKeys.url.rawValue]
 		authorFlairCSSClass <- map[LinkKeys.authorFlairCSSClass.rawValue]
+		authorFlairText <- map[LinkKeys.authorFlairText.rawValue]
+		clicked <- map[LinkKeys.clicked.rawValue]
+		domain <- map[LinkKeys.domain.rawValue]
+		hidden <- map[LinkKeys.hidden.rawValue]
+		isSelf <- map[LinkKeys.isSelf.rawValue]
+		linkFlairCSSClass <- map[LinkKeys.linkFlairCSSClass.rawValue]
+		linkFlairText <- map[LinkKeys.linkFlairText.rawValue]
+		locked <- map[LinkKeys.locked.rawValue]
+		numComments <- map[LinkKeys.numComments.rawValue]
+		over18 <- map[LinkKeys.over18.rawValue]
+		permalink <- map[LinkKeys.permalink.rawValue]
+		preview <- map[LinkKeys.preview.rawValue]
+		saved <- map[LinkKeys.saved.rawValue]
+		score <- map[LinkKeys.score.rawValue]
+		selftext <- map[LinkKeys.selftext.rawValue]
+		selftextHTML <- map[LinkKeys.selftextHTML.rawValue]
+		subredditNamePrefixed <- map[LinkKeys.subredditNamePrefixed.rawValue]
+		stickied <- map[LinkKeys.stickied.rawValue]
+		subreddit <- map[LinkKeys.subreddit.rawValue]
+		subredditID <- map[LinkKeys.subredditID.rawValue]
+		title <- map[LinkKeys.title.rawValue]
 		thumbnail <- map[LinkKeys.thumbnail.rawValue]
 		thumbnailHeight <- map[LinkKeys.thumbnailHeight.rawValue]
-		preview <- map[LinkKeys.preview.rawValue]
+		url <- map[LinkKeys.url.rawValue]
 
 		// Votable
-		ups <- map[VotableKeys.ups.rawValue]
 		downs <- map[VotableKeys.downs.rawValue]
 		likes <- map[VotableKeys.likes.rawValue]
+		ups <- map[VotableKeys.ups.rawValue]
 
 		// Created
 		created <- map[CreatedKeys.created.rawValue]

ec5f8a60617bf4b26a5c961056407dabfbd1b77f
Ron Daryl Magno <ron.zohan@gmail.com>
2017-10-26 22:02:13 +0800

    Removed whitespace

--- RedditTests/Modules/DataLayer/Endpoints/ListingEndpointTest.swift
+++ RedditTests/Modules/DataLayer/Endpoints/ListingEndpointTest.swift
@@ -10,17 +10,17 @@ import XCTest
 @testable import Reddit
 
 class ListingEndpointTest: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-	
+
 	func testListingEndpointWithParams() {
 		// Given I have parameters
 		let parameters: [String: Any] = ["subreddit": "Dota2"]
 		let expectedUrl = "https://www.reddit.com/r/Dota2/hot"
-		
+
 		// When I set the parameters of listing endpoint
 		let listingEndpoint = ListingHotEndpoint()
 		listingEndpoint.parameters = parameters
@@ -28,16 +28,16 @@ class ListingEndpointTest: XCTestCase {
 		// Then the new url should be the expected url
 		XCTAssertEqual(listingEndpoint.url, expectedUrl)
 	}
-	
+
 	func testListingEndpointWithAdditionalParameters() {
 		// Given I have parameters
 		let parameters: [String: Any] = ["subreddit": "Dota2", "limit": 14]
 		let expectedUrl = "https://www.reddit.com/r/Dota2/hot?limit=14"
-		
+
 		// When I set the parameters of listing endpoint
 		let listingEndpoint = ListingHotEndpoint()
 		listingEndpoint.parameters = parameters
-		
+
 		// Then the new url should be the expected url
 		XCTAssertEqual(listingEndpoint.url, expectedUrl)
 	}

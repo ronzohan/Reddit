fff1c7bee68111e42a8e74fe266cd7f40ccbf63a
Ron Daryl Magno <ron.zohan@gmail.com>
2017-11-01 01:24:16 +0800

    Added test for ListingDatasource

--- Reddit/Modules/Listings/Datasource/ListingDatasource.swift
+++ Reddit/Modules/Listings/Datasource/ListingDatasource.swift
@@ -7,9 +7,53 @@
 //
 
 import Foundation
+import RxDataSources
 
 class ListingDatasource {
-	init(listing: Listing) {
-		
+	class func configureLinkTableViewCell(cell: BaseLinkTableViewCell, link: Link) {
+		let viewModel = LinkCellViewModel(link: link)
+
+		cell.viewModel = viewModel
+		cell.configure()
+	}
+
+	static func datasource() -> RxTableViewSectionedReloadDataSource<LinkSectionModel> {
+		return RxTableViewSectionedReloadDataSource<LinkSectionModel>
+			.init(configureCell: { (dataSource, tableView, indexPath, _) -> UITableViewCell in
+
+				let cell: BaseLinkTableViewCell?
+				let link: Link
+
+				switch dataSource[indexPath] {
+					case .urlLink(let _link):
+						cell = urlLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+						link = _link
+					case .imageLink(let _link):
+						cell = imageLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+						link = _link
+				}
+
+				guard let unwrappedCell = cell else {
+					return UITableViewCell()
+				}
+
+				configureLinkTableViewCell(cell: unwrappedCell, link: link)
+
+				return unwrappedCell
+		})
+	}
+
+	class func urlLinkTableViewCellFor(tableView: UITableView,
+	                                   indexPath: IndexPath) -> UrlLinkTableViewCell? {
+		let cell: UrlLinkTableViewCell? = tableView.dequeueReusableCell(forIndexPath: indexPath)
+
+		return cell
+	}
+
+	class func imageLinkTableViewCellFor(tableView: UITableView,
+	                                     indexPath: IndexPath) -> ImageLinkTableViewCell? {
+		let cell: ImageLinkTableViewCell? = tableView.dequeueReusableCell(forIndexPath: indexPath)
+
+		return cell
 	}
 }
--- RedditTests/Modules/Listing/Datasource/ListingDatasourceTest.swift
+++ RedditTests/Modules/Listing/Datasource/ListingDatasourceTest.swift
@@ -7,22 +7,122 @@
 //
 
 import XCTest
+import RxSwift
+import RxTest
+import RxDataSources
 @testable import Reddit
 
 class ListingDatasourceTest: XCTestCase {
-	func testDatasourceInit() {
-		let link1 = Link()
-		link1.title = "A 19 year old Sofia Vergara"
-		link1.subredditNamePrefixed = "r/pics"
-		link1.createdUTC = 1506946298
-		link1.domain = "i.imgur.com"
+
+	lazy var tableView: UITableView = {
+		return UITableView()
+	}()
+
+	var disposeBag: DisposeBag!
+
+	override func setUp() {
+		super.setUp()
+
+		tableView.register(UrlLinkTableViewCell.self,
+		                   forCellReuseIdentifier: UrlLinkTableViewCell.reuseIdentifier)
+		tableView.register(ImageLinkTableViewCell.self,
+		                   forCellReuseIdentifier: ImageLinkTableViewCell.reuseIdentifier)
+		tableView.dataSource = self
 		
-		let listing = Listing()
-		listing.children = [link1]
-		let _ = ListingDatasource(listing: listing)
+		disposeBag = DisposeBag()
 	}
-	
-	func testDatasourceNumberOfSections() {
-		
+
+	func testDatasource() {
+		class ListingDatasourceMock: ListingDatasource {
+			static var didConfigureLinkTableViewCell = false
+			override class func configureLinkTableViewCell(cell: BaseLinkTableViewCell, link: Link) {
+				super.configureLinkTableViewCell(cell: cell, link: link)
+				didConfigureLinkTableViewCell = true
+			}
+		}
+
+		// Given
+		let sut = ListingDatasourceMock.datasource()
+		let urlLink = Link()
+		urlLink.postHint = .link
+
+		let imageLink = Link()
+		imageLink.postHint = .image
+
+		let sections: [LinkSectionModel] = [
+			LinkSectionModel.linkSection(items: [
+				SectionItem.urlLink(link: urlLink),
+				SectionItem.imageLink(link: imageLink)
+			])
+		]
+
+		let indexPath = IndexPath(row: 0, section: 0)
+
+		// When
+		Observable.just(sections)
+			.bind(to: tableView.rx.items(dataSource: sut))
+			.addDisposableTo(disposeBag)
+
+		tableView.cellForRow(at: indexPath)
+
+		// Then
+		XCTAssertTrue(ListingDatasourceMock.didConfigureLinkTableViewCell)
+	}
+
+	func testLinkTableCellIsConfigured() {
+		class MockCell: BaseLinkTableViewCell {
+			var didConfigure = false
+			override func configure() {
+				didConfigure = true
+			}
+		}
+
+		// Given
+		let cell = MockCell()
+		let link = Link()
+
+		// When
+		ListingDatasource.configureLinkTableViewCell(cell: cell, link: link)
+
+		// Then
+		XCTAssertTrue(cell.didConfigure)
+	}
+
+	func testUrlLinkTableViewCell() {
+		// Given
+		let indexPath = IndexPath(row: 0, section: 0)
+		let cell: UrlLinkTableViewCell?
+
+		// When
+		cell = ListingDatasource.urlLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+
+		// Then
+		XCTAssertNotNil(cell)
+	}
+
+	func testImageLinkTableViewCell() {
+		// Given
+		let indexPath = IndexPath(row: 0, section: 0)
+		let cell: ImageLinkTableViewCell?
+
+		// When
+		cell = ListingDatasource.imageLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+
+		// Then
+		XCTAssertNotNil(cell)
+	}
+}
+
+extension ListingDatasourceTest: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
 	}
 }

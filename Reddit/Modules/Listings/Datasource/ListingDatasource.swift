0cded3901812df82f90269d061bf95f15fb00445
Ron Daryl Magno <ron.zohan@gmail.com>
2017-11-06 01:32:45 +0800

    Moved logic for table view datasource into its own file

--- Reddit/Modules/Listings/Datasource/ListingDatasource.swift
+++ Reddit/Modules/Listings/Datasource/ListingDatasource.swift
@@ -9,10 +9,16 @@
 import Foundation
 import RxDataSources
 
-class ListingDatasource {
+class ListingDatasource: NSObject {
+
+	var dataSource: [ListingSection] = []
 
 	private var onLoadNextPage: (() -> Void)?
-	func onLoadNextPage(completion: @escaping () -> Void) {
+	private var isNextPageQueried: Bool = false
+
+	var estimatedHeightCache: [[CGFloat]] = []
+
+	func onLoadNextPage(completion: (() -> Void)?) {
 		onLoadNextPage = completion
 	}
 
@@ -23,68 +29,102 @@ class ListingDatasource {
 		cell.configure()
 	}
 
-	func datasource() -> RxTableViewSectionedReloadDataSource<LinkSectionModel> {
-		return RxTableViewSectionedReloadDataSource<LinkSectionModel>
-			.init(configureCell: { (dataSource, tableView, indexPath, _) -> UITableViewCell in
-
-				let cell: BaseLinkTableViewCell?
-				let link: Link
-
-				switch dataSource[indexPath] {
-					case .urlLink(let _link):
-						cell = self.urlLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
-						link = _link
-					case .imageLink(let _link):
-						cell = self.imageLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
-						link = _link
-				}
-
-				guard let unwrappedCell = cell else {
-					return UITableViewCell()
-				}
-
-				self.configureLinkTableViewCell(cell: unwrappedCell, link: link)
-
-				return unwrappedCell
-		})
-	}
-//
-//	/**
-//	Load the next page of the subreddit if the current index path that it
-//	is loading surpass the threshold
-//	*/
-//	func loadNextPage(withCurrentIndexpath indexPath: IndexPath) {
-//		if viewModel.shouldLoadNextPage(withIndexPath: indexPath) {
-//
-//			showLoadingNextPage()
-//
-//			viewModel.getListingNextPage()
-//				.drive(onCompleted: {
-//					self.dismissLoadingView()
-//
-//					self.listingTableView.reloadData()
-//				})
-//				.addDisposableTo(disposeBag)
-//		}
-//	}
-//
-//	func showLoadingNextPage() {
-//		self.listingTableView.tableFooterView = loadingIndicator
-//
-//		loadingIndicator.startAnimating()
-//	}
-
 	func urlLinkTableViewCellFor(tableView: UITableView,
-	                                   indexPath: IndexPath) -> UrlLinkTableViewCell? {
+	                             indexPath: IndexPath) -> UrlLinkTableViewCell? {
 		let cell: UrlLinkTableViewCell? = tableView.dequeueReusableCell(forIndexPath: indexPath)
-		
+
 		return cell
 	}
-	
+
 	func imageLinkTableViewCellFor(tableView: UITableView,
-	                                     indexPath: IndexPath) -> ImageLinkTableViewCell? {
+	                               indexPath: IndexPath) -> ImageLinkTableViewCell? {
 		let cell: ImageLinkTableViewCell? = tableView.dequeueReusableCell(forIndexPath: indexPath)
-		
+
 		return cell
+	}
+
+	func shouldLoadNextPage(withIndexPath indexPath: IndexPath) -> Bool {
+		if case ListingSection.linkRows(let links) = dataSource[indexPath.section],
+			indexPath.row == links.count - 1 &&
+			!dataSource.isEmpty &&
+			indexPath.section == dataSource.count - 1 &&
+			!isNextPageQueried {
+			return true
+		} else {
+			return false
+		}
 	}
 }
+
+// MARK: - Datasource
+extension ListingDatasource: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return dataSource.count
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		switch dataSource[section] {
+			case let .linkRows(links):
+				return links.count
+		}
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		switch dataSource[indexPath.section] {
+			case let .linkRows(links: links):
+				return linkCellForLink(link: links[indexPath.row], tableView: tableView, forRowAt: indexPath)
+		}
+	}
+
+	func linkCellForLink(link: Link,
+	                     tableView: UITableView,
+	                     forRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell: BaseLinkTableViewCell?
+
+		switch link.postHint {
+		case .link:
+			cell = self.urlLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+		case .image:
+			cell = self.imageLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+		default:
+			cell = self.urlLinkTableViewCellFor(tableView: tableView, indexPath: indexPath)
+		}
+
+		guard let linkCell = cell else {
+			return UITableViewCell()
+		}
+
+		self.configureLinkTableViewCell(cell: linkCell, link: link)
+
+		return linkCell
+	}
+
+	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		if shouldLoadNextPage(withIndexPath: indexPath) {
+			onLoadNextPage?()
+		}
+	}
+
+	func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+		if estimatedHeightCache.count > indexPath.section {
+			if estimatedHeightCache[indexPath.section].count > indexPath.row {
+				return estimatedHeightCache[indexPath.section][indexPath.row]
+			}
+		}
+		return 100
+	}
+
+	func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell,
+	               forRowAt indexPath: IndexPath) {
+
+		if estimatedHeightCache.count <= indexPath.section {
+			estimatedHeightCache.append([cell.frame.height])
+		} else if estimatedHeightCache[indexPath.section].count <= indexPath.row {
+			estimatedHeightCache[indexPath.section].append(cell.frame.height)
+		} else {
+			estimatedHeightCache[indexPath.section][indexPath.row] = cell.frame.height
+		}
+	}
+}
+
+extension ListingDatasource: UITableViewDelegate {}
--- Reddit/Modules/Listings/ViewModel/ListingViewModel.swift
+++ Reddit/Modules/Listings/ViewModel/ListingViewModel.swift
@@ -11,12 +11,9 @@ import RxSwift
 import RxCocoa
 
 class ListingViewModel: NSObject {
-	var listingSections = Variable<[ListingSection]>([])
-
 	private var after: String?
 	private var before: String?
 	private var subreddit: String = ""
-	private var isNextPageQueried: Bool = false
 
 	var useCase: ListingUseCase
 
@@ -24,65 +21,40 @@ class ListingViewModel: NSObject {
 		self.useCase = useCase
 	}
 
-	func numberOfSections() -> Int {
-		return listingSections.value.count
-	}
-
-	func numberOfRowsInSection(section: Int) -> Int {
-		return listingSections.value[section].rowCount
-	}
-
-	func getListing(subreddit: String) -> Driver<Listing> {
+	func getListing(subreddit: String) -> Observable<ListingSection> {
 		return self.useCase.fetchHotListing(subreddit: subreddit, after: nil, before: nil)
 			.do(onNext: { (listing) in
-				self.listingSections.value = self.createListingSections(withListing: listing)
-
 				// Update current after and before values for loading next page
 				self.after = listing.after
 				self.before = listing.before
 				self.subreddit = subreddit
 			})
-			.asDriver(onErrorJustReturn: Listing())
+			.map({ (listing) -> ListingSection in
+				let section = self.sectionModels(forListing: listing)
 
+				return section
+			})
 	}
 
-	func getListingNextPage() -> Driver<Listing> {
-
-		isNextPageQueried = true
-
-		return useCase.fetchHotListing(subreddit: subreddit, after: after, before: before)
+	func getListingNextPage() -> Observable<ListingSection> {
+		return self.useCase.fetchHotListing(subreddit: subreddit, after: after, before: before)
 			.do(onNext: { (listing) in
-				let section = self.createListingSections(withListing: listing)
-				self.listingSections.value.append(contentsOf: section)
-
 				// Update current after and before values for loading next page
 				self.after = listing.after
 				self.before = listing.before
-
-				self.isNextPageQueried = false
 			})
-			.asDriver(onErrorJustReturn: Listing())
+			.map({ (listing) -> ListingSection in
+				let section = self.sectionModels(forListing: listing)
+
+				return section
+			})
 	}
 
-	func createListingSections(withListing listing: Listing) -> [ListingSection] {
-		let links: [Link] = listing.children.flatMap({ (link) -> Link? in
-			return link as? Link
-		})
+	func sectionModels(forListing listing: Listing) -> ListingSection {
+		let links = listing.children.flatMap { (thing) -> Link? in
+						return thing as? Link
+					}
 
-		var sections: [ListingSection] = []
-		sections.append(LinkSection(links: links))
-
-		return sections
-	}
-
-	func shouldLoadNextPage(withIndexPath indexPath: IndexPath) -> Bool {
-		if !listingSections.value.isEmpty &&
-			indexPath.section == listingSections.value.count - 1 &&
-			indexPath.row == listingSections.value[indexPath.section].rowCount - 1 &&
-			!isNextPageQueried {
-			return true
-		} else {
-			return false
-		}
+		return ListingSection.linkRows(links: links)
 	}
 }

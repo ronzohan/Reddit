85b4ec33b8b9b59998f07972e1db94141d0873d6
Ron Daryl Magno <ron.zohan@gmail.com>
2018-01-26 00:12:25 +0800

    Added comment mapping

--- Reddit/Modules/DataLayer/Models/Comment.swift
+++ Reddit/Modules/DataLayer/Models/Comment.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Comment: Votable, Created, Thing {
+struct Comment: Votable, Created, Thing, Codable {
     let archived: Bool
     let author: String
     let bannedAtUTC: UInt64?
@@ -28,18 +28,92 @@ struct Comment: Votable, Created, Thing {
     let id: String
     let isSubmitter: Bool
     let likes: Bool?
-    let linkID: String = "t3_7qri8q"
-    let modReasonTitle: String? = "Some title" //?
-    let name: String = "t1_dsrhuxt"
-    let parentID: String = "t1_dsrhtrt"
-    let permalink: String = "/r/gaming/comments/7qri8q/had_a_20_minute_costume_competition_i_knew_from/dsrhuxt/"
-    let removalReason: String? = "nothing" //?
-    let saved: Bool = true
-    let score: Int = 76
-    let scoreHidden: Bool = true
-    let stickied: Bool = true
-    let subreddit: String = "gaming"
-    let subredditNamePrefixed: String = "r/gaming"
-    let ups: Int = 10
-    let subredditID: String = "t5_2qh03"
+    let linkID: String
+    let modReasonTitle: String?
+    let name: String
+    let parentID: String
+    let permalink: String
+    let removalReason: String?
+    let saved: Bool
+    let score: Int
+    let scoreHidden: Bool
+    let stickied: Bool
+    let subreddit: String
+    let subredditNamePrefixed: String
+    let ups: Int
+    let subredditID: String
+    
+    enum CodingKeys: String, CodingKey {
+        case archived
+        case author
+        case bannedAtUTC = "banned_at_utc"
+        case bannedBy = "banned_by"
+        case body
+        case bodyHTML = "body_html"
+        case canGild = "can_gild"
+        case canModPost = "can_mod_post"
+        case collapsed
+        case controversiality
+        case created
+        case createdUTC = "created_utc"
+        case depth
+        case downs
+        case edited
+        case gilded
+        case id
+        case isSubmitter = "is_submitter"
+        case likes
+        case linkID = "link_id"
+        case modReasonTitle = "mod_reason_title"
+        case name
+        case parentID = "parent_id"
+        case permalink
+        case removalReason = "removal_reason"
+        case saved
+        case score
+        case scoreHidden = "score_hidden"
+        case stickied
+        case subreddit
+        case subredditNamePrefixed = "subreddit_name_prefixed"
+        case ups
+        case subredditID = "subreddit_id"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        archived = try container.decode(Bool.self, forKey: .archived)
+        author = try container.decode(String.self, forKey: .author)
+        bannedAtUTC = try? container.decode(UInt64.self, forKey: .bannedAtUTC)
+        bannedBy = try? container.decode(String.self, forKey: .bannedBy)
+        body = try container.decode(String.self, forKey: .body)
+        bodyHTML = try container.decode(String.self, forKey: .bodyHTML)
+        canGild = try container.decode(Bool.self, forKey: .canGild)
+        canModPost = try container.decode(Bool.self, forKey: .canModPost)
+        collapsed = try container.decode(Bool.self, forKey: .collapsed)
+        controversiality = try container.decode(Int.self, forKey: .controversiality)
+        created = try container.decode(UInt64.self, forKey: .created)
+        createdUTC = try container.decode(UInt64.self, forKey: .createdUTC)
+        depth = try container.decode(Int.self, forKey: .depth)
+        downs = try container.decode(Int.self, forKey: .downs)
+        edited = try container.decode(Bool.self, forKey: .edited)
+        gilded = try container.decode(Int.self, forKey: .gilded)
+        id = try container.decode(String.self, forKey: .id)
+        isSubmitter = try container.decode(Bool.self, forKey: .isSubmitter)
+        likes = try? container.decode(Bool.self, forKey: .likes)
+        linkID = try container.decode(String.self, forKey: .linkID)
+        modReasonTitle = try? container.decode(String.self, forKey: .modReasonTitle)
+        name = try container.decode(String.self, forKey: .name)
+        parentID = try container.decode(String.self, forKey: .parentID)
+        permalink = try container.decode(String.self, forKey: .permalink)
+        removalReason = try? container.decode(String.self, forKey: .removalReason)
+        saved = try container.decode(Bool.self, forKey: .saved)
+        score = try container.decode(Int.self, forKey: .score)
+        scoreHidden = try container.decode(Bool.self, forKey: .scoreHidden)
+        stickied = try container.decode(Bool.self, forKey: .stickied)
+        subreddit = try container.decode(String.self, forKey: .subreddit)
+        subredditNamePrefixed = try container.decode(String.self, forKey: .subredditNamePrefixed)
+        ups = try container.decode(Int.self, forKey: .ups)
+        subredditID = try container.decode(String.self, forKey: .subredditID)
+    }
 }
--- RedditTests/Modules/DataLayer/Model/CommentTest.swift
+++ RedditTests/Modules/DataLayer/Model/CommentTest.swift
@@ -15,9 +15,42 @@ class CommentTest: XCTestCase {
         let commentData = CommentDataMock.CommentData
         
         // When
-        //let comment: Comment? = DictionaryHelper.model(for: commentData)
+        guard let comment: Comment = DictionaryHelper.model(for: commentData) else {
+            XCTFail("Failed to parse comment data")
+            return
+        }
         
         // Then
-        //XCTAssertEqual(comment, <#T##expression2: [Equatable]##[Equatable]#>)
+        XCTAssertEqual(comment.archived, CommentDataMock.archived)
+        XCTAssertEqual(comment.author, CommentDataMock.author)
+        XCTAssertEqual(comment.bannedAtUTC, CommentDataMock.bannedAtUTC)
+        XCTAssertEqual(comment.bannedBy, CommentDataMock.bannedBy)
+        XCTAssertEqual(comment.body, CommentDataMock.body)
+        XCTAssertEqual(comment.bodyHTML, CommentDataMock.bodyHTML)
+        XCTAssertEqual(comment.canGild, CommentDataMock.canGild)
+        XCTAssertEqual(comment.canModPost, CommentDataMock.canModPost)
+        XCTAssertEqual(comment.collapsed, CommentDataMock.collapsed)
+        XCTAssertEqual(comment.controversiality, CommentDataMock.controversiality)
+        XCTAssertEqual(comment.created, CommentDataMock.created)
+        XCTAssertEqual(comment.createdUTC, CommentDataMock.createdUTC)
+        XCTAssertEqual(comment.depth, CommentDataMock.depth)
+        XCTAssertEqual(comment.downs, CommentDataMock.downs)
+        XCTAssertEqual(comment.edited, CommentDataMock.edited)
+        XCTAssertEqual(comment.gilded, CommentDataMock.gilded)
+        XCTAssertEqual(comment.id, CommentDataMock.id)
+        XCTAssertEqual(comment.isSubmitter, CommentDataMock.isSubmitter)
+        XCTAssertEqual(comment.linkID, CommentDataMock.linkID)
+        XCTAssertEqual(comment.modReasonTitle, CommentDataMock.modReasonTitle)
+        XCTAssertEqual(comment.name, CommentDataMock.name)
+        XCTAssertEqual(comment.parentID, CommentDataMock.parentID)
+        XCTAssertEqual(comment.permalink, CommentDataMock.permalink)
+        XCTAssertEqual(comment.removalReason, CommentDataMock.removalReason)
+        XCTAssertEqual(comment.saved, CommentDataMock.saved)
+        XCTAssertEqual(comment.score, CommentDataMock.score)
+        XCTAssertEqual(comment.scoreHidden, CommentDataMock.scoreHidden)
+        XCTAssertEqual(comment.subreddit, CommentDataMock.subreddit)
+        XCTAssertEqual(comment.subredditNamePrefixed, CommentDataMock.subredditNamePrefixed)
+        XCTAssertEqual(comment.ups, CommentDataMock.ups)
+        XCTAssertEqual(comment.subredditID, CommentDataMock.subredditID)
     }
 }

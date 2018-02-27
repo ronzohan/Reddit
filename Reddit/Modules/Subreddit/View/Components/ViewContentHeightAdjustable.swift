08267454d330bd91176896290a914c06b2a86d3e
Ron Daryl Magno <ron.zohan@gmail.com>
2018-02-27 23:04:56 +0800

    Added protocol for views that has adjustable content height

---
+++ Reddit/Modules/Subreddit/View/Components/ViewContentHeightAdjustable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewHeightContentable.swift
+//  Reddit
+//
+//  Created by Ron Daryl Magno on 2/27/18.
+//  Copyright © 2018 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewContentHeightAdjustable {
+    func setContentHeight(height: Double)
+}

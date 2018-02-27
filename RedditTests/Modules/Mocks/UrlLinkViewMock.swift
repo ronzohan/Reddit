a42ace85d5a0db5ea58961fd7ebe2bca90ca188f
Ron Daryl Magno <ron.zohan@gmail.com>
2018-02-27 23:39:06 +0800

    Added mock for url link view

---
+++ RedditTests/Modules/Mocks/UrlLinkViewMock.swift
@@ -0,0 +1,32 @@
+//
+//  UrlLinkViewMock.swift
+//  RedditTests
+//
+//  Created by Ron Daryl Magno on 2/27/18.
+//  Copyright © 2018 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+@testable import Reddit
+
+class UrlLinkViewMock: UrlLinkViewable {
+    var didSetImage = false
+    func setImage(withUrl url: String) {
+        didSetImage = true
+    }
+    
+    var didSetTitle = false
+    func setTitle(withTitle title: String) {
+        didSetTitle = true
+    }
+
+    var didSetUps = false
+    func setUps(withUps ups: String) {
+        didSetUps = true
+    }
+    
+    var didSetMeta = false
+    func setMeta(withMeta meta: String) {
+        didSetMeta = true
+    }
+}
